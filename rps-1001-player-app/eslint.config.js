0243a2f318785691895d2903ddb989401ff137aa
FuiHung <namgikfuihung@gmail.com>
2024-08-30 21:59:38 -0700

    Add libraries and refine linting config

--- rps-1001-player-app/eslint.config.js
+++ rps-1001-player-app/eslint.config.js
@@ -1,5 +1,6 @@
 import js from '@eslint/js'
 import globals from 'globals'
+import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
@@ -32,6 +33,11 @@ export default tseslint.config(
         'warn',
         { allowConstantExport: true },
       ],
+      'semi': ['error', 'never'],
+      'function-paren-newline': ['error', 'multiline'],
+      "indent": ["error", 2],
+      "react/prop-types": "off",
+      "eol-last": ["error", "always"],
     },
   },
 )
